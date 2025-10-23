203eb102e636e99a3fb5233930a899272ca8b06b
trishamaemallari43-ops <trishamaemallari43@gmail.com>
2025-10-23 06:13:13 +0000

    Fix models to use ObjectIdField for Djongo compatibility

--- octofit-tracker/backend/octofit_tracker/models.py
+++ octofit-tracker/backend/octofit_tracker/models.py
@@ -1,6 +1,7 @@
 from djongo import models
 
 class Team(models.Model):
+    id = models.ObjectIdField(primary_key=True, editable=False)
     name = models.CharField(max_length=100, unique=True)
     class Meta:
         db_table = 'teams'
@@ -8,6 +9,7 @@ class Team(models.Model):
         return self.name
 
 class User(models.Model):
+    id = models.ObjectIdField(primary_key=True, editable=False)
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='members')
@@ -17,6 +19,7 @@ class User(models.Model):
         return self.name
 
 class Activity(models.Model):
+    id = models.ObjectIdField(primary_key=True, editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='activities')
     type = models.CharField(max_length=100)
     duration = models.IntegerField()  # in minutes
@@ -27,6 +30,7 @@ class Activity(models.Model):
         return f"{self.user.name} - {self.type}"
 
 class Workout(models.Model):
+    id = models.ObjectIdField(primary_key=True, editable=False)
     name = models.CharField(max_length=100)
     description = models.TextField()
     suggested_for = models.ManyToManyField(Team, related_name='workouts')
@@ -36,6 +40,7 @@ class Workout(models.Model):
         return self.name
 
 class Leaderboard(models.Model):
+    id = models.ObjectIdField(primary_key=True, editable=False)
     team = models.OneToOneField(Team, on_delete=models.CASCADE, related_name='leaderboard')
     points = models.IntegerField(default=0)
     class Meta:
